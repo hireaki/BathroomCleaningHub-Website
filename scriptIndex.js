cc401a7109a5b07d35399a0e9b9797da4435e694
hireaki <hireaki30@gmail.com>
2024-09-29 23:21:24 +0800

    Update scriptIndex.js

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,8 +1,11 @@
-var ask = confirm("Would You Like To Subscribe To Our Newsletter?\n\nRecieve Updates About New Guides, Latest Products, And Exclusive Tips And Tricks To Help You Keep Your Bathroom Tidy.");
+if (!localStorage.getItem("subscribed")) {
+    var ask = confirm("Would You Like To Subscribe To Our Newsletter?\n\nReceive Updates About New Guides, Latest Products, And Exclusive Tips And Tricks To Help You Keep Your Bathroom Tidy.");
 
-if(ask == true){
-    var email = prompt("Enter Your Email");
-    if(email != null){
-        alert("Thank You For Subscribing!");
+    if (ask) {
+        var email = prompt("Enter Your Email");
+        if (email != null) {
+            alert("Thank You For Subscribing!");
+            localStorage.setItem("subscribed", true); // Store the flag so the alert doesn't show again
+        }
     }
 }
