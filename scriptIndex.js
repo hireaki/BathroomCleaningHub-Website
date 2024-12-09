2923cc86218fb21eaad318d3c0c4b0f55d005d6a
hireaki <hireaki30@gmail.com>
2024-12-09 20:44:59 +0800

    hamburg nav

--- scriptIndex.js
+++ scriptIndex.js
@@ -9,3 +9,10 @@ if (!localStorage.getItem("subscribed")) {
         }
     }
 }
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
